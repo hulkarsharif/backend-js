a899acf1126cdb516fc239e3ef720cb76e5b94be
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-09-08 10:19:47 -0400

    Finish the assignment-1

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -18,19 +18,7 @@ class FinancialAnalytics {
             writeCallback("newData.json", transformedData);
         });
     }
-    // }
-    // totalIncome(content) {
-    //     const dataObj = content;
-    //     let total = 0;
-    //     for (const incomes in dataObj) {
-    //         total += incomes;
-    //         console.log(content);
-    //     }
-    // }
 
-    static calculateMonthlySavings(data) {
-        return [1, 2, 3, 4, 5];
-    }
     init(fileUrl, transformedDataFunc) {
         FinancialAnalytics.readJSONData(
             fileUrl,
@@ -39,42 +27,82 @@ class FinancialAnalytics {
         );
     }
 
-    // calculateMonthlySavings(data) {
-    //     let monthlySavings = {};
+    static calculateMonthlySavings(data) {
+        let monthlySavings = {};
 
-    //     for (const month in data) {
-    //         monthlySavings[month] = {};
-    //         for (const category in content[month].incomes) {
-    //             const income = content[month].incomes[category];
-    //             const expenses = content[month].expenses[category];
-    //             const totally =
-    //                 income -
-    //                 (expenses.food +
-    //                     expenses.rent +
-    //                     expenses.entertainment +
-    //                     expenses.transportation);
-    //             monthlySavings[month][category] = totally;
-    //         }
-    //     }
-    //     return monthlySavings;
-    // }
-    // compareMonthlyExpenses(data) {
-    //     let monthlyExp = {};
-    //     for (let monthly in data[1].expenses) {
-    //         monthlyExp[monthly] = {};
-    //         const items = [food, rent, entertainment, transportation];
-    //         for (let k in items) {
-    //             let maxExp = { id: 1, amount: data[1].expenses[monthly][k] };
-    //             let minExp = { id: 1, amount: data[1].expenses[monthly][k] };
-    //         }
-    //     }
-    // }
+        for (const month in data) {
+            monthlySavings[month] = {};
+            for (const category in data[month].incomes) {
+                const income = data[month].incomes[category];
+                const expenses = data[month].expenses[category];
+                const totally =
+                    income -
+                    (expenses.food +
+                        expenses.rent +
+                        expenses.entertainment +
+                        expenses.transportation);
+                monthlySavings[month][category] = totally;
+            }
+        }
+        return monthlySavings;
+    }
+    static compareMonthlyExpenses(data) {
+        let monthlyExp = {};
+        for (let monthly in data[1].expenses) {
+            monthlyExp[monthly] = {};
+
+            // const items = [food, rent, entertainment, transportation];
+            for (let season in data[1].expenses[monthly]) {
+                let maxExp = {
+                    id: 1,
+                    amount: data[1].expenses[monthly][season]
+                };
+                let minExp = {
+                    id: 1,
+                    amount: data[1].expenses[monthly][season]
+                };
+
+                for (let num in data) {
+                    var expense = data[num].expenses[monthly][season];
+                }
+                if (expense > maxExp.amount) {
+                    maxExp.id = monthly.id;
+                    maxExp.amount = expense;
+                }
+                if (expense < minExp.amount) {
+                    minExp.id = monthly.id;
+                    minExp.amount = expense;
+                }
+                monthlyExp[monthly][season] = {
+                    max: maxExp,
+                    min: minExp
+                };
+            }
+        }
+
+        return monthlyExp;
+    }
+    static calculateAnnualExpenses(data) {
+        let annualExp = {};
+        for (let personId in data) {
+            annualExp[personId] = {};
+            for (const category in data[personId].expenses[
+                Object.keys(data[personId].expenses)[0]
+            ]) {
+                let isTotal = 0;
+                for (const month in data[personId].expenses) {
+                    isTotal = +data[personId].expenses[month][category];
+                }
+                annualExp[personId][category] = isTotal;
+            }
+        }
+        return annualExp;
+    }
 }
 const analytics = new FinancialAnalytics();
 
-analytics.init("data.json", FinancialAnalytics.calculateMonthlySavings);
+analytics.init("data.json", FinancialAnalytics.compareMonthlyExpenses);
 
-// // console.log(data.readFile());
 // // // console.log(data.readJSONData());
 // console.log(FinancialAnalytics.calculateMonthlySavings());
-// console.log(data.calculateMonthlySavings());
+// console.log(analytics.readJSONData());
